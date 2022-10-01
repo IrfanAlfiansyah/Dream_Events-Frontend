7c516da1ead93dfb3e3af90f81b632bc0442c4bb
IrfanAlfiansyah <irfan.alfiansyah1995@gmail.com>
2022-10-02 00:25:36 +0700

    up

--- src/index.js
+++ src/index.js
@@ -1,8 +1,40 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "./index.css";
-import App from "./App";
-import reportWebVitals from "./reportWebVitals";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import Home from "./pages/Home";
+import Signup from "./pages/Signup";
+import Signin from "./pages/Signin";
+import Profile from "./pages/Profile";
+import Order from "./pages/Order";
+import ForgotPassword from "./pages/ForgotPassword";
+import EventDetail from "./pages/EventDetail";
+import Payment from "./pages/Payment";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/signin" element={<Signin />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/order" element={<Order />} />
+        <Route path="/forgotpassword" element={<ForgotPassword />} />
+        <Route path="/productdetail" element={<EventDetail />} />
+        <Route path="/payment" element={<Payment />} />
+        <Route
+          path="*"
+          element={
+            <main className="d-flex justify-content-center align-item-center p-5 fw-bold">
+              <p>Page Not Found!</p>
+            </main>
+          }
+        />
+      </Routes>
+    </BrowserRouter>
+  );
+}
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
@@ -10,8 +42,3 @@ root.render(
     <App />
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
