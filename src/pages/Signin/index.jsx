aa524baf6a4c999a6274f10d37de5bf4f4548788
IrfanAlfiansyah <irfan.alfiansyah1995@gmail.com>
2022-10-03 17:49:02 +0700

    add signin

--- src/pages/Signin/index.jsx
+++ src/pages/Signin/index.jsx
@@ -1,63 +1,50 @@
 import React from "react";
-import { useState } from "react";
-import axios from "../../utils/axios";
-import { useNavigate } from "react-router-dom";
+import icon from "../../assets/img/icon.png";
+import logo from "../../assets/img/logo.png";
+import google from "../../assets/img/google.png";
+import facebook from "../../assets/img/fb..png";
+
+import "./Signin.css";
 
 export default function Signin() {
-  const navigate = useNavigate();
-  const [form, setForm] = useState({
-    email: "",
-    password: "",
-  });
-
-  const [showPassword, setShowPassword] = useState(false);
-
-  const handlelogin = async () => {
-    try {
-      const result = await axios.post("auth/login", form);
-      localStorage.setItem("idUser", result.data.data.id);
-      localStorage.setItem("token", result.data.data.token);
-      alert(result.data.msg);
-      navigate("/signup");
-    } catch (error) {
-      console.error(error.response);
-    }
-  };
-
-  const handleChangeForm = (e) => {
-    setForm({ ...form, [e.target.name]: e.target.value });
-  };
-  const hanldeShowPassword = () => {
-    setShowPassword(!showPassword);
-  };
-
   return (
-    <div className="container text-center">
-      <h1>Login Page</h1>
-      <hr />
-      <input
-        type="email"
-        placeholder="Input your email ..."
-        name="email"
-        onChange={handleChangeForm}
-      />
-      {""}
-      <br />
-      <input
-        type={showPassword ? "text" : "password"}
-        placeholder="Input your password ..."
-        name="password"
-        onChange={handleChangeForm}
-      />
-      {""}
-      <br />
-      <button className="btn btn-secondary" onClick={hanldeShowPassword}>
-        {showPassword ? "Hide" : "Show"} Password
-      </button>
-      <br />
-      <button className="btn btn-primary" onClick={handlelogin}>
-        Signin
-      </button>
-    </div>
+    <main>
+      <aside className="side-content-signin align-self-center text-center">
+        <img className="side-img-signin " src={icon} alt="icon" />
+      </aside>
+      <section className="main-header">
+        <header className="header">
+          <img className="header-logo" src={logo} alt="logo" />
+        </header>
+        <div className="title">
+          <h1 className="title-text">Sign In</h1>
+          <div className="welcome">
+            <p className="welcome-text">Hi, Welcome back to Urticket! </p>
+          </div>
+        </div>
+        <div className="form">
+          <form className="form-container">
+            <input type="text" className="form-input" placeholder="Username" />
+            <input type="text" className="form-input" placeholder="Email" />
+            <input
+              type="password"
+              className="form-input"
+              placeholder="Password"
+            />
+            <button className="forgot-pw">Forgot Password?</button>
+            <button className="button-signin">Sign In</button>
+            <p className="alternative-signin">or sign in with</p>
+            <div className="alternative-signin-icon">
+              <button className="alternative-signin-button">
+                <img src={google} alt="google" />
+              </button>
+              <button className="alternative-signin-button">
+                <img src={facebook} alt="facebook" />
+              </button>
+            </div>
+          </form>
+        </div>
+      </section>
+    </main>
   );
 }
